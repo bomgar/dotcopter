ddeb65f4fc424adf9627a6e417f3d0e6ac0b6653
Patrick Haun <patrick@bomgar.email>
2019-09-06 13:20:08 +0200

    [wip] release build

--- package.sh
+++ package.sh
@@ -6,12 +6,12 @@ if [ "$TRAVIS" = true ]; then
     chmod -R 777 .
 fi
 
-docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:1.37.0 cargo build --release
+docker run --rm -i -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder:1.37.0 cargo build --release
 name=dotcopter
 target_dir="target/x86_64-unknown-linux-musl/release"
 
 version=$($target_dir/$name --version | cut -d ' ' -f 2)
 
-docker run -v $(readlink -e ./$target_dir):/release -it --rm  alanfranz/fpm-within-docker:centos-7     fpm -s dir -t rpm -n $name -p /release/$name.rpm -v $version /release/$name=/usr/bin/$name
-docker run -v $(readlink -e ./$target_dir):/release -it --rm  alanfranz/fpm-within-docker:ubuntu-zesty fpm -s dir -t deb -n $name -p /release/$name.deb -v $version /release/$name=/usr/bin/$name
+docker run -v $(readlink -e ./$target_dir):/release -i --rm  alanfranz/fpm-within-docker:centos-7     fpm -s dir -t rpm -n $name -p /release/$name.rpm -v $version /release/$name=/usr/bin/$name
+docker run -v $(readlink -e ./$target_dir):/release -i --rm  alanfranz/fpm-within-docker:ubuntu-bionic fpm -s dir -t deb -n $name -p /release/$name.deb -v $version /release/$name=/usr/bin/$name
 
